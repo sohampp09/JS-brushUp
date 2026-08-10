3117db83b8356a579da1146e1fa08474af44312f
sohampatil3000@gmail.com <sohampatil3000@gmail.com>
2026-08-10 14:52:11 +0530

    adding intro folder

---
+++ 1.intro/1.intro.js
@@ -0,0 +1,35 @@
+//JS VARIABLES AND CONSTANT:
+
+ //JS variables
+
+let num = 5;
+//here, num is stores then number 5.
+//------------------------------------------------------------------------------//
+
+
+//Declare variable in js
+ //In JavaScript, we use the var or let keywords to declare variables. For example,
+var age;
+let name; //here age and name is variables.
+
+// Initialize Variables in JavaScript
+// We use the assignment operator = to assign a value to a variable.
+
+const num1;
+
+num1 = 10; //here 5 assingned to the variable num1.
+console.log(num1); 
+
+
+// Change the Value of Variables
+// The value of a variable may vary. Hence, the name variable.
+
+// Let's look at the example below to learn how to change the value of a variable:
+
+// assign 5 to variable score
+let score = 5; 
+console.log(score); // 5
+
+// change the value of score to 3
+score = 3; 
+console.log(score); // 3
---
+++ 1.intro/2.console_log.js
@@ -0,0 +1,31 @@
+// JavaScript console.log()
+// In JavaScript, the console.log() method displays messages or variables in the browser's console.
+
+console.log('message'); //prints the string 'message' to the console.
+console.log(2000);     //prints the number 2000 to the console
+
+
+//ex2: print the values stored in variables
+
+const greet = "speedy";
+console.log(greet);
+
+
+
+
+// Combine Strings and Variables
+// In JavaScript, you can combine strings and variables in console.log() using the following methods:
+// 1. Using Substitution Strings
+
+
+console.log("Hi there am", greet)
+
+
+// 2. Using Template Literals
+
+// We enclose a message inside two backticks ` ` to utilize template literals. For example,
+const greet2 = "speedy live!"
+
+const live = ` HEYYY guysss welcome back to the ${greet2}`;
+
+console.log(live);
